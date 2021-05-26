ffc516c483455628ae7aa6832755da9842e040f6
twicks95 <teguh.wck95@gmail.com>
2021-05-26 21:41:45 +0700

    Add fontawesome.js

---
+++ src/fontawesome.js
@@ -0,0 +1,8 @@
+// import the library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faGoogle, faEye, faEyeSlash);
