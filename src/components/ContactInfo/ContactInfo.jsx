017256e0b4e6004232ffa5027b3722f416ee55fd
twicks95 <teguh.wck95@gmail.com>
2021-05-31 08:39:45 +0700

    Update contact info component

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -1,32 +1,64 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from "./ContactInfo.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-import Avatar5 from "../../assets/images/user5.png";
 import Chat from "../../assets/icons/Chat.svg";
 
+import Avatar5 from "../../assets/images/user5.png";
+import Img1 from "../../assets/images/img1.jpg";
+import Img2 from "../../assets/images/img2.jpg";
+import Img3 from "../../assets/images/img3.jpg";
+import Img4 from "../../assets/images/img4.jpg";
+import Img5 from "../../assets/images/img5.jpg";
+import Img6 from "../../assets/images/img6.jpg";
+import Img7 from "../../assets/images/img7.jpg";
+import Img8 from "../../assets/images/img8.jpg";
+import Img9 from "../../assets/images/img9.jpg";
+import Img10 from "../../assets/images/img10.jpg";
+
 function ContactInfo(props) {
-  // const [open, setOpen] = useState(false);
+  const [location, setLocation] = useState(false);
+  const [image, setImage] = useState(true);
+  const [documents, setDocuments] = useState(false);
+
+  const handleLocation = () => {
+    setLocation(true);
+    setImage(false);
+    setDocuments(false);
+  };
+  const handleImage = () => {
+    setLocation(false);
+    setImage(true);
+    setDocuments(false);
+  };
+  const handleDocuments = () => {
+    setLocation(false);
+    setImage(false);
+    setDocuments(true);
+  };
 
   return (
-    <div className={`${styles.barContainer}`}>
+    <div
+      className={`${props.showInfo ? "" : styles.hide} ${styles.barContainer}`}
+    >
       <div
         className={`d-flex align-items-center justify-content-center ${styles.headBar}`}
       >
         <FontAwesomeIcon
-          icon={faChevronLeft}
-          className={`${styles.chevronLeft} ${styles.textColorPrimary}`}
+          icon={faChevronRight}
+          className={`${styles.chevronRight} ${styles.textColorPrimary}`}
+          onClick={props.handleShowInfo}
         />
         <h2 className={`m-0 ${styles.textColorPrimary}`}>@mmldolg</h2>
       </div>
       <div
-        className={`border d-flex flex-column align-items-center ${styles.profileContainer}`}
+        className={`d-flex flex-column align-items-center ${styles.profileContainer}`}
       >
         <img src={Avatar5} alt="avatar" className={styles.avatar} />
-        <div className="border border-danger w-100">
+        <div className="w-100">
           <Row className={styles.detailInfo}>
             <Col
               xs={12}
@@ -44,15 +76,97 @@ function ContactInfo(props) {
             </Col>
           </Row>
         </div>
-        <div className={`border border-success w-100`}>
+        <div className={`w-100`}>
           <div className={`${styles.navigation}`}>
             <ul
               className={`d-flex align-items-center justify-content-between m-0`}
             >
-              <li>Location</li>
-              <li className={`${styles.active}`}>Image</li>
-              <li>Documents</li>
+              <li
+                className={`${location ? styles.active : ""}`}
+                onClick={handleLocation}
+              >
+                Location
+              </li>
+              <li
+                className={`${image ? styles.active : ""}`}
+                onClick={handleImage}
+              >
+                Image
+              </li>
+              <li
+                className={`${documents ? styles.active : ""}`}
+                onClick={handleDocuments}
+              >
+                Documents
+              </li>
             </ul>
+          </div>
+          <div className={`${styles.tabulate} mt-4 mb-4`}>
+            <Row xs={3} className="gy-2 gx-0">
+              <Col>
+                <img src={Img1} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img2} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img3} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img4} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img5} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img6} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img7} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img8} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img9} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img10} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img6} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img7} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img8} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img1} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img2} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img3} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img7} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img8} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img9} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img10} alt="img" className={`${styles.image}`} />
+              </Col>
+              <Col>
+                <img src={Img6} alt="img" className={`${styles.image}`} />
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
