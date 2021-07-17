9b61eeb6ad2e4476bbec4c2f2a41b4c56f8f0c44
twicks95 <teguh.wck95@gmail.com>
2021-07-17 20:13:47 +0700

    Update chat page

--- src/pages/main/Chat/Chat.jsx
+++ src/pages/main/Chat/Chat.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import styles from "./Chat.module.css";
 import Leftbar from "../../../components/Leftbar/Leftbar";
